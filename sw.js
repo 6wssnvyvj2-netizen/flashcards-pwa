f5ebdb6ec68ee4dab2bed8950e0885091b260875
6wssnvyvj2-netizen <6wssnvyvj2@privaterelay.appleid.com>
2025-12-02 18:57:22 -0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,24 @@
+const CACHE_NAME = "flashcards-brainscape-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/cards.json" 
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("flashcards-cache").then(cache => {
-      return cache.addAll(["/", "/index.html", "/manifest.json"]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then(resp => {
+      // Return cached file if found, otherwise fetch from network
+      return resp || fetch(event.request);
+    })
   );
 });
